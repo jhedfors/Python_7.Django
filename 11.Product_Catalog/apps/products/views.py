33fb83850c5d09955a47bed4d0fd3a55a7bb44fd
Jeff Hedfors <git.j@hedfors.net>
2016-06-01 05:34:20 -0700

    update

--- 11.Product_Catalog/apps/products/views.py
+++ 11.Product_Catalog/apps/products/views.py
@@ -32,10 +32,3 @@ def edit(request):
 def delete(request,id):
     models.delete(id)
     return redirect('/products')
-@register.filter(name='get_value_from_dict')
-def get_value_from_dict(dict_data, key):
-    """
-    usage example {{ your_dict|get_value_from_dict:your_key }}
-    """
-    if key:
-        return dict_data.get(key)
